353ef10ce4f0b6b6ba2c24b122fcd22dfcec45a1
mac <mac@macdeMacBook-Pro-2.local>
2018-04-17 14:12:45 +0800

    ...

---
+++ src/base/title/title.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div class="title">
+      <i class="icon-back" @click.stop="back"></i>
+      <h1>{{title}}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    back () {
+      this.$router.back()
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+.title {
+  text-align: center;
+  height: 50px;
+  line-height: 50px;
+  background: rgba(243, 107, 67, 0.8);
+  display: flex;
+
+  i {
+    width: 50px;
+    line-height: 50px;
+    font-size: 20px;
+    color: white;
+  }
+
+  h1 {
+    width: 75%;
+    color: white;
+    font-size: 20px;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,11 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueLazyload from 'vue-lazyload'
 import './config/rem'
 
 import 'common/stylus/index.styl'
 
 Vue.config.productionTip = false
+
+Vue.use(VueLazyload, {
+  loading: require('common/image/default.jpg')
+})
 
 /* eslint-disable no-new */
 new Vue({
